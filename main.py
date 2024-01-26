9bbc677c0ac05deb24ca03c0e1d6c79b427e2f19
bdaza <brian.daza@proton.me>
2024-01-26 16:02:37 +0100

    version 1.1 of open space organizer brian

--- main.py
+++ main.py
@@ -2,29 +2,17 @@ import pandas as pd
 from src.openspace import OpenSpace
 from random import sample
 
-
-
-
-# testing here
-# names = [
-#     "Peter", "Patrick", "Lea", "Dorothy",
-#     "Lenny", "Richie", "Chaim", "Maja",
-#     "Raffael", "Cormac", "Kirsty", "Norman",
-#     "Rosario", "Mar", "Merida", "Luke",
-#     "Robert", "Ari", "Ali", "Sander",
-#     "Kiki", "Mo", "Jad", "Jabari"]
-
-
 # put the main code here
 if __name__ == "__main__":
-    #path = input("Please provide a filepath to the list of colleagues: ")
-    path = "/Users/ibex/becode/openspace-organizer/names.xlsx"
+    path = input("Please provide a filepath to the list of colleagues: ")
+    #path = "/Users/ibex/becode/openspace-organizer/colleagues.xlsx"
     df = pd.read_excel(path, header = None)
     names = df.iloc[:,0].tolist()
     space = OpenSpace(number_of_tables=6)
     space.organize(names)
    
     def help():
+        # Prints out the options 
         print("Options: ")
         print("0 exit")
         print("1 display different tables and their occupants")
@@ -33,6 +21,7 @@ if __name__ == "__main__":
 
 
     while True:
+        # Runs a loop to give user options 
         print()
         help()
         option = int(input("Pick option: "))
@@ -45,7 +34,3 @@ if __name__ == "__main__":
             print(f"Free seats in room: {space.free_seats()}")
         elif option == 3:
             print(f"There are {space.persons_in_room()} persons in the room")
-        # elif option == 4:
-        #     names = sample(names, len(names))
-        #     space.organize(names)
-        #     print("Reshuffling...")
